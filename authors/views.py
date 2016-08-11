ed0fc865fe700498132905ad787ff0bf67a29de6
didoogan <tzd0409@gmail.com>
2016-08-11 17:49:58 +0300

    messages without specifications

--- authors/views.py
+++ authors/views.py
@@ -2,6 +2,8 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
+from django.contrib import messages
+
 
 from books.models import Author
 from .forms import AuthorForm
@@ -21,8 +23,12 @@ class AuthorCreateView(CreateView):
     model = Author
     form_class = AuthorForm
     success_url = reverse_lazy('authors:list_view')
-    # fields = ['first_name', 'last_name']
     template_name = 'authors/create_author.html'
+
+    def form_valid(self, form):
+        message = super(AuthorCreateView, self).form_valid(form)
+        messages .success(self.request, u'Author has been successfully created')
+        return message
 
 
 class AuthorDeleteView(DeleteView):
@@ -30,9 +36,20 @@ class AuthorDeleteView(DeleteView):
     success_url = reverse_lazy('authors:list_view')
     template_name = 'authors/delete_view.html'
 
+    success_message = u'Author has been successfully deleted'
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(AuthorDeleteView, self).delete(request, *args, **kwargs)
+
 
 class AuthorUpdateView(UpdateView):
     model = Author
     form_class = AuthorForm
     success_url = reverse_lazy('authors:list_view')
     template_name = 'authors/update_view.html'
+
+    def form_valid(self, form):
+        message = super(AuthorUpdateView, self).form_valid(form)
+        messages .success(self.request, u'Author has been successfully edited')
+        return message
--- books/views.py
+++ books/views.py
@@ -2,6 +2,8 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import redirect
+from django.contrib import messages
 
 from books.models import Book
 
@@ -23,6 +25,11 @@ class BookCreateView(CreateView):
             form.fields[field].widget.attrs.update({'class': 'form-control'})
         return form
 
+    def form_valid(self, form):
+        message = super(BookCreateView, self).form_valid(form)
+        messages .success(self.request, u'Book has been successfully created')
+        return message
+
 
 class BookDetailView(DetailView):
     model = Book
@@ -41,11 +48,21 @@ class BookUpdateView(UpdateView):
             form.fields[field].widget.attrs.update({'class': 'form-control'})
         return form
 
+    def form_valid(self, form):
+        message = super(BookUpdateView, self).form_valid(form)
+        messages .success(self.request, u'Book has been successfully edited')
+        return message
+
 
 class BookDeleteView(DeleteView):
     model = Book
     success_url = reverse_lazy('books:list_view')
     template_name = 'books/delete_book.html'
+    success_message = u'Book has been successfully deleted'
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(BookDeleteView, self).delete(request, *args, **kwargs)
 
 
 
