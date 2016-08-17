d8c7873f6b4ceeae62005d6298cdb6ca8653dab3
didoogan <tzd0409@gmail.com>
2016-08-17 22:54:57 +0300

    index url was added

--- library/urls.py
+++ library/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from books.views import BookListView
+
 
 urlpatterns = [
+    url(r'^$', BookListView.as_view(),  name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^books/', include('books.urls', namespace='books')),
     url(r'^authors/', include('authors.urls', namespace='authors')),
